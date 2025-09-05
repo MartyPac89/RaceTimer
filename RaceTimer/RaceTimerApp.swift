e9b96cd7c3d9542c8fd7755fd127d3314e615b5b
Martin Pacourek <pacourekm@gmail.com>
2025-09-05 21:31:32 +0200

    Initial Commit

---
+++ RaceTimer/RaceTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceTimerApp.swift
+//  RaceTimer
+//
+//  Created by Marty Pac on 05.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct RaceTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RaceTimerTests/RaceTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RaceTimerTests.swift
+//  RaceTimerTests
+//
+//  Created by Marty Pac on 05.09.2025.
+//
+
+import Testing
+@testable import RaceTimer
+
+struct RaceTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
